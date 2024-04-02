423a25cef3cd4723976666bd26c54e7301e50023
Broodymmm <142622779+Broodymmm@users.noreply.github.com>
2024-04-02 12:02:27 -0700

    challenge 5

--- Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -22,6 +22,7 @@ public class GameManagerX : MonoBehaviour
     private float spaceBetweenSquares = 2.5f; 
     private float minValueX = -3.75f; //  x value of the center of the left-most square
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
     public void StartGame(int difficulty)
